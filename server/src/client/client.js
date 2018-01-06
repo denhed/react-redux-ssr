ea8aa434547a0d6f7d2f513195ad834665934ce6
denhed <dennis.hedlund@me.com>
2018-01-06 20:44:55 +0100

    client, creating custom axios instance to pass into redux-thunk

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -10,10 +10,14 @@ import { renderRoutes } from 'react-router-config';
 import Routes from './Routes';
 import reducers from './reducers';
 
+const axiosInstance = axios.create({
+   baseURL: '/api'
+});
+
 const store = createStore(
     reducers,
     window.INITIAL_STATE,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk.withExtraArgument(axiosInstance))
 );
 
 
