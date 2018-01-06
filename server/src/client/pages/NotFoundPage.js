8d174c2bcfe2d8daa2c9f57346f6b21b9ea9cfb1
denhed <dennis.hedlund@me.com>
2018-01-06 23:53:11 +0100

    setting staticContext and send status 404.

--- server/src/client/pages/NotFoundPage.js
+++ server/src/client/pages/NotFoundPage.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const NotFoundPage = () => {
+const NotFoundPage = ({ staticContext = {} }) => {
+
+    // server will check this value and send status(404).
+    staticContext.notFound = true;
+
     return <h1>Ooops, route not found.</h1>
 };
 
