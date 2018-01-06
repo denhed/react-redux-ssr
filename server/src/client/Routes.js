c139f0127e0b0fc33a2362f8115a66635b1573d2
denhed <dennis.hedlund@me.com>
2018-01-07 00:09:35 +0100

    added AdminsListPage. when testing this state you must be logged in otherwise you will recive error messages in server console.

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -3,6 +3,7 @@ import App from './App';
 import HomePage from './pages/HomePage';
 import UsersListPage from './pages/UsersListPage';
 import NotFoundPage from './pages/NotFoundPage';
+import AdminsListPage from './pages/AdminsListPage';
 
 export default [
     {
@@ -15,6 +16,9 @@ export default [
             }, {
                 ...UsersListPage,
                 path: '/users'
+            }, {
+                ...AdminsListPage,
+                path: '/admins'
             }, {
                 ...NotFoundPage
             }
