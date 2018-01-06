246119fe403e72d9f8cd8af3e37e43c3b5808f83
denhed <dennis.hedlund@me.com>
2018-01-07 00:51:07 +0100

    Solution to Promise all failures. We will render the page when all promises are resolved, it doesn't matter if one request fails (one single promise reject) we still render the page. If we only use promise.all the page will render at the first request fail  and we don't want that.

--- server/src/index.js
+++ server/src/index.js
@@ -20,28 +20,41 @@ app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-   // passing in req to let createStore to get access to cookie on the request.
-   const store = createStore(req);
+    // passing in req to let createStore to get access to cookie on the request.
+    const store = createStore(req);
 
-   //console.log(matchRoutes(Routes, req.path));
-   // calls loadData function in our components, passing in server store
-   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
-      return route.loadData ? route.loadData(store) : null;
-   });
+    //console.log(matchRoutes(Routes, req.path));
+    // calls loadData function in our components, passing in server store
+    // null is return if there are pages with no loadData function.
+    // we will render the page when all promises are resolved, it doesn't matter
+    // if one request fails (one single promise reject) we still render the page.
+    // If we only use promise.all the page will render at the first request fail
+    // and we don't want that.
+    const promises = matchRoutes(Routes, req.path)
+        .map(({ route }) => {
+            return route.loadData ? route.loadData(store) : null;
+        })
+        .map(promise => {
+            if(promise) {
+                return new Promise((resolv, reject) => {
+                    promise.then(resolv).catch(resolv);
+                });
+            }
+        });
 
-   Promise.all(promises)
-       .then(() => {
-           const context = {};
-           const content = renderer(req, store, context);
-           // if NotFoundPage component is rendered it will set notFound to true.
-           // will only send 404 if navigate directly to a non existing page, if
-           // navigate within app (Link) it is not sending request to server.
-           if(context.notFound) {
-               res.status(404);
-           }
+    Promise.all(promises)
+        .then(() => {
+            const context = {};
+            const content = renderer(req, store, context);
+            // if NotFoundPage component is rendered it will set notFound to true.
+            // will only send 404 if navigate directly to a non existing page, if
+            // navigate within app (Link) it is not sending request to server.
+            if(context.notFound) {
+                res.status(404);
+            }
 
-           res.send(content);
-       });
+            res.send(content);
+        });
 
 }); // allow all routes
 
