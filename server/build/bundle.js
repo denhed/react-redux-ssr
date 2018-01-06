2722f72e2af2d8bb717f3de702646ecb6a4791a8
denhed <dennis.hedlund@me.com>
2018-01-06 13:15:16 +0100

    serialize server state to prevent xss attacks when we put react state in windows obejct. serialize change special characters (< / ...) to unicode which isn't executed on runtime

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -159,6 +159,10 @@ var _reactRedux = __webpack_require__(11);
 
 var _reactRouterConfig = __webpack_require__(18);
 
+var _serializeJavascript = __webpack_require__(21);
+
+var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
 var _Routes = __webpack_require__(7);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -181,7 +185,7 @@ exports.default = function (req, store) {
     ));
 
     // tell browser to go back till express and get public bundle.js
-    return '\n      <html>\n         <head></head>\n         <body>\n            <div id="root">' + content + '</div>\n            <script>\n                window.INITIAL_STATE = ' + JSON.stringify(store.getState()) + '\n            </script>   \n            <script src="bundle.js"></script>\n         </body>\n      </html>\n   ';
+    return '\n      <html>\n         <head></head>\n         <body>\n            <div id="root">' + content + '</div>\n            <script>\n                window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + '\n            </script>   \n            <script src="bundle.js"></script>\n         </body>\n      </html>\n   ';
 };
 
 /***/ }),
@@ -519,6 +523,12 @@ exports.default = {
     loadData: loadData,
     component: (0, _reactRedux.connect)(mapStateToProps, { fetchUsers: _actions.fetchUsers })(UsersList)
 };
+
+/***/ }),
+/* 21 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
 
 /***/ })
 /******/ ]);
